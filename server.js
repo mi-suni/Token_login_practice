ff046891c4734f30a1ab8c87265e24de53481a8b
mi-suni <mi88250819@gmail.com>
2024-09-03 18:09:15 +0900

    node.js 4일차 완료

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://127.0.0.1:[본인의 포트번호]",
-      "http://localhost:[본인의 포트번호]",
+      "http://127.0.0.1:5500",
+      "http://localhost:5500",
     ],
     methods: ["OPTIONS", "POST", "GET", "DELETE"],
     credentials: true,
@@ -40,19 +40,17 @@ app.post("/", (req, res) => {
   if (!userInfo) {
     res.status(401).send("로그인 실패");
   } else {
-    // 1. 유저정보가 있는 경우 accessToken을 발급하는 로직을 작성하세요.(sign)
-    // 이곳에 코드를 작성하세요.
-    // 2. 응답으로 accessToken을 클라이언트로 전송하세요. (res.send 사용)
-    // 이곳에 코드를 작성하세요.
+    const accessToken = jwt.sign({userId: userInfo.user_id}, secretKey, {expiresIn: 1000*60*10})
+    res.send(accessToken)
   }
 });
 
 // 클라이언트에서 get 요청을 받은 경우
 app.get("/", (req, res) => {
-  // 3. req headers에 담겨있는 accessToken을 검증하는 로직을 작성하세요.(verify)
-  // 이곳에 코드를 작성하세요.
-  // 4. 검증이 완료되면 유저정보를 클라이언트로 전송하세요.(res.send 사용)
-  // 이곳에 코드를 작성하세요.
+  const accessToken = req.headers.authorization.split(' ')[1]
+  const payload = jwt.verify(accessToken, secretKey)
+  const userInfo = users.find(el => el.user_id === payload.userId)
+  return res.send(userInfo)
 });
 
 app.listen(3000, () => console.log("서버 실행!"));
